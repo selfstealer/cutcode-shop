2d5ab1b0866cd5162c09e257b8f006352f030696
selfstealer <selfstealer@gmail.com>
2023-10-20 23:02:43 +0300

    stage 2 down

--- app/Traits/Models/HasSlug.php
+++ app/Traits/Models/HasSlug.php
@@ -5,21 +5,31 @@ declare(strict_types=1);
 namespace App\Traits\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Stringable;
 
 trait HasSlug
 {
+    protected int $slugNumber = 0;
+
     protected static function bootHasSlug()
     {
         static::creating(function (Model $model) {
-            $model->slug = $model->slug
-                ?? str($model->{self::slugFrom()})
-                    ->append(microtime(true)) // time() всё равно генерит ошибки TODO дз генаратор уникального имени
-                    ->slug();
+            $model->slug = $model->slug ?? $model->generateUniqueSlug(self::slugFrom());
         });
     }
 
     public static function slugFrom(): string
     {
         return 'title';
+    }
+
+    protected function generateUniqueSlug(string $slugFrom): Stringable
+    {
+        do {
+            $slug = str($this->{$slugFrom} . ($this->slugNumber === 0 ? '' : ' ' . $this->slugNumber))->slug();
+            $this->slugNumber++;
+        } while ($this->newModelQuery()->where('slug', '=', $slug)->exists());
+
+        return $slug;
     }
 }
